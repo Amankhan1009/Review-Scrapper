921affaf00a1ba59996d75a3f495bd4566c088ab
Amankhan1009 <amankhan34356@gmail.com>
2025-08-02 21:40:39 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="myntra-scrapper",
+    version="0.0.1",
+    author="Md Aman Alam",
+    author_email="amankhan34356@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
